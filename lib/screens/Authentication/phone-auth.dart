e7df0d405b954b08d7c49e7251a81c5949bcca0a
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-08-28 22:47:21 +0545

    field validation adn continue button

--- lib/screens/Authentication/phone-auth.dart
+++ lib/screens/Authentication/phone-auth.dart
@@ -1,67 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class PhoneAuthPage extends StatelessWidget {
+class PhoneAuthPage extends StatefulWidget {
   const PhoneAuthPage({Key? key}) : super(key: key);
 
+  @override
+  _PhoneAuthPageState createState() => _PhoneAuthPageState();
+}
+
+class _PhoneAuthPageState extends State<PhoneAuthPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: Scaffold(
-        // resizeToAvoidBottomInset: false,
-        backgroundColor: Color(0xffF6F5FA),
-        body: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/images/Logo.png",
-                  height: 60,
-                ),
-                SizedBox(height: 15),
-                Text(
-                  "CORDSPOT",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "location based anynomous\nchating application",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey,
-                  ),
-                ),
-                SizedBox(height: 50),
-                Form(
-                  child: Container(
-                    // padding: EdgeInsets.symmetric(vertical: 5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          // resizeToAvoidBottomInset: false,
+          backgroundColor: Color(0xffF6F5FA),
+          body: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/images/Logo.png",
+                      height: 60,
                     ),
-                    child: TextFormField(
-                      style: TextStyle(height: 1.6),
+                    SizedBox(height: 15),
+                    Text(
+                      "CORDSPOT",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "location based anynomous\nchating application",
                       textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        filled: false,
-                        hintText: "Phone Number",
-                        hintStyle: TextStyle(
-                          color: Color(0xffCACACA),
-                          fontSize: 17,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Form(
+                      key: _formKey,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 2),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                        ),
+                        child: TextFormField(
+                          validator: (val) => val!.length < 10
+                              ? "Enter a valid Phone number"
+                              : null,
+                          style: TextStyle(height: 1.6),
+                          keyboardType: TextInputType.phone,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(
+                              RegExp('[0-9+]+'),
+                            ),
+                            LengthLimitingTextInputFormatter(15),
+                          ],
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            filled: false,
+                            hintText: "Phone Number",
+                            hintStyle: TextStyle(
+                              color: Color(0xffCACACA),
+                              fontSize: 17,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                )
-              ],
+                    SizedBox(height: 30),
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 50,
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {}
+                      },
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      color: Color(0xffFBD737),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
