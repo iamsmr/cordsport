719b8752b458db3bfda1b53933f420cfaf82faea
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-08-29 07:17:04 +0545

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cordspot',
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: CustomRouter.onGenerateRoute,
       initialRoute: "/wrapper",
       theme: ThemeData(
---
+++ lib/screens/Authentication/verification-page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class VerificationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      
+    );
+  }
+}
